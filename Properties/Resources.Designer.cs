9bc78510bcf1b3d60cd15001aee6a1d0da14093b
FireHeartMaster <eduardo.octavio@hotmail.com>
2022-08-20 00:08:06 +0200

    Added icon to project

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -1164,6 +1164,16 @@ namespace EmbroideryCreator.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon EmbroideryCreatorIcon {
+            get {
+                object obj = ResourceManager.GetObject("EmbroideryCreatorIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap EraserIcon {
